a4606a0079081058212f81695e9b7d73bd999a12
benyoung96 <benyoung@iastate.edu>
2019-02-23 20:04:30 -0600

    Added a scripts folder that contains scripts to populate the local database with schedules, teams, players

---
+++ scripts/postSchedule.sh
@@ -0,0 +1,289 @@
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Orioles","datetime": "2019-03-28 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Orioles","datetime": "2019-03-30 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Orioles","datetime": "2019-03-31 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Tigers","datetime": "2019-04-01 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Tigers","datetime": "2019-04-02 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Tigers","datetime": "2019-04-03 03:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+      curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Orioles","away": "Yankees","datetime": "2019-04-04 02:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+        curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Orioles","away": "Yankees","datetime": "2019-04-06 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+        curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Orioles","away": "Yankees","datetime": "2019-04-07 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+        curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Astros","away": "Yankees","datetime": "2019-04-08 06:10:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Astros","away": "Yankees","datetime": "2019-04-09 07:10:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Astros","away": "Yankees","datetime": "2019-04-10 06:40:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "White Sox","datetime": "2019-04-12 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "White Sox","datetime": "2019-04-13 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+            curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "White Sox","datetime": "2019-04-14 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+              curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Red Sox","datetime": "2019-04-16 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Red Sox","datetime": "2019-04-17 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Royals","datetime": "2019-04-18 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Royals","datetime": "2019-04-19 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Royals","datetime": "2019-04-20 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Royals","datetime": "2019-04-21 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Angles","away": "Yankees","datetime": "2019-04-22 09:07:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                      curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Angles","away": "Yankees","datetime": "2019-04-23 09:07:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                      curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Angles","away": "Yankees","datetime": "2019-04-24 09:07:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                      curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Angles","away": "Yankees","datetime": "2019-04-25 08:07:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                      curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Giants","away": "Yankees","datetime": "2019-04-26 09:15:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                      curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Giants","away": "Yankees","datetime": "2019-04-27 03:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                        curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Giants","away": "Yankees","datetime": "2019-04-28 03:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                        curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "D-backs","away": "Yankees","datetime": "2019-04-30 08:40:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "D-backs","away": "Yankees","datetime": "2019-05-01 02:40:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Twins","datetime": "2019-05-03 05:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                            curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Twins","datetime": "2019-05-04 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                            curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Twins","datetime": "2019-05-05 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                              curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Mariners","datetime": "2019-05-06 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Mariners","datetime": "2019-05-07 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Mariners","datetime": "2019-05-08 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Mariners","datetime": "2019-05-09 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Rays","away": "Yankees","datetime": "2019-05-10 06:10:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Rays","away": "Yankees","datetime": "2019-05-11 05:10:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Rays","away": "Yankees","datetime": "2019-05-12 12:10:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Orioles","datetime": "2019-05-13 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Orioles","datetime": "2019-05-14 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Orioles","datetime": "2019-05-15 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                      curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Rays","datetime": "2019-05-17 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                        curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Rays","datetime": "2019-05-18 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                        curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Rays","datetime": "2019-05-19 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Orioles","away": "Yankees","datetime": "2019-05-20 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Orioles","away": "Yankees","datetime": "2019-05-21 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Orioles","away": "Yankees","datetime": "2019-05-22 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Orioles","away": "Yankees","datetime": "2019-05-23 11:35:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                          curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Royals","away": "Yankees","datetime": "2019-05-24 07:15:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                            curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Royals","away": "Yankees","datetime": "2019-05-25 01:15:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                              curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Royals","away": "Yankees","datetime": "2019-05-26 01:15:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                              curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Padres","datetime": "2019-05-27 12:15:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+                                              curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Padres","datetime": "2019-05-28 05:35:00"}' \
+  http://localhost:8080/api/yankees/schedule  
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Padres","datetime": "2019-05-29 12:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Red Sox","datetime": "2019-05-30 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
+
+    curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"home": "Yankees","away": "Red Sox","datetime": "2019-05-31 06:05:00"}' \
+  http://localhost:8080/api/yankees/schedule
---
+++ scripts/postTeams.sh
@@ -0,0 +1,149 @@
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Yankees","location": "New York","ballpark": "Yankee Stadium", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Orioles","location": "Baltimore","ballpark": "Oriole Park at Camden Yards", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Red Sox","location": "Boston","ballpark": "Fenway Park", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "White Sox","location": "Chicago","ballpark": "Guaranteed Rate Field", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Indians","location": "Cleveland","ballpark": "Progressive Field", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Tigers","location": "Detroit","ballpark": "Comerica Park", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Astros","location": "Houston","ballpark": "Minute Maid Park", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Royals","location": "Kansas City","ballpark": "Kauffman Stadium", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Angels","location": "Los Angeles","ballpark": "Angel Stadium", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Twins","location": "Minnesota","ballpark": "Target Field", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Athletics","location": "Oakland","ballpark": "Oakland Coliseum", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Mariners","location": "Seattle","ballpark": "T-Mobile Park", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Rays","location": "Tampa Bay","ballpark": "Tropicana Field", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Rangers","location": "Texas","ballpark": "Globe Life Park in Arlington", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Blue Jays","location": "Toronto","ballpark": "Rogers Centre", "league": "American"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Diamondbacks","location": "Arizona","ballpark": "Chase Field", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Braves","location": "Atlanta","ballpark": "SunTrust Park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Cubs","location": "Chicago","ballpark": "Wrigley Field", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Reds","location": "Cincinnati","ballpark": "Great American Ball Park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Rockies","location": "Colorado","ballpark": "Coors Field", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Dodgers","location": "Los Angeles","ballpark": "Dodger Stadium", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Marlins","location": "Miami","ballpark": "Marlins Park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Brewers","location": "Milwaukee","ballpark": "Miller Park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Mets","location": "New York","ballpark": "Citi Field", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Phillies","location": "Philadelphia","ballpark": "Citizens Bank Park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Pirates","location": "Pittsburgh","ballpark": "PNC Park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Padres","location": "San Diego","ballpark": "Petco Park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Giants","location": "San Francisco","ballpark": "Oracle park", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Cardinals","location": "St. Louis","ballpark": "Busch Stadium", "league": "National"}' \
+  http://localhost:8080/api/teams
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name": "Nationals","location": "Washington","ballpark": "Nationals Park", "league": "National"}' \
+  http://localhost:8080/api/teams
